09e1f84882a106bec5cf84861a61b51f144a1d7b
Charles Brands <charles.brands@gmail.com>
2017-11-07 20:34:58 +0100

    added statusbar

---
+++ components/MyStatusBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StatusBar, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+import { purple} from '../utils/colors';
+
+const styles = StyleSheet.create({
+    myStatusBar: {
+        backgroundColor: purple,
+        height: Constants.statusBarHeight
+    }
+})
+
+export default function MyStatusBar() {
+    return (
+        <View style={styles.myStatusBar}>
+            <StatusBar translucent style={styles.myStatusBar} barStyle='light-content' />
+        </View>
+    )
+}
+
+// export default function MyStatusBar () {
+//     return (
+//         <View style={styles.myStatusBar}>
+//             <StatusBar backgroundColor={purple} />
+//         </View>
+//     )
+// }
